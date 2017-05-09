6794cbe52959ad0f541f4e8495a7f6be7bee2bc1
a0922056105 <a0922056105@gmail.com>
2017-05-09 06:28:42 -0400

    finish add book feature

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,30 @@
+class BooksController < ApplicationController
+  def index 
+    @books=Book.all
+  end
+  
+  def new
+    @book=Book.new
+  end
+  
+  def show
+   @book=Book.find_by(id: params[:id])
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to books_path
+    else
+      render 'new'
+    end  
+  end
+  
+  def edit
+  end
+  
+  
+   def book_params
+    params.require("book").permit(:title, :intro, :author, :price)
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+  validates :title,presence: true
+  has_many :logs
+  
+=begin
+  def display_name
+    "#{title}(#{author})"
+  end
+=end
 end
