ac00bd86c989edc2cdd64fd47ad324c416b8ab86
a0922056105 <a0922056105@gmail.com>
2017-05-10 09:59:07 -0400

    add midterm

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,17 @@
 class BooksController < ApplicationController
+  before_action :find_book,only:[:show, :edit, :update, :destroy]
   def index 
     @books=Book.all
+  end
+    
+  def show
+   @book=Book.find_by(id: params[:id])
   end
   
   def new
     @book=Book.new
   end
-  
-  def show
-   @book=Book.find_by(id: params[:id])
-  end
+
   
   def create
     @book = Book.new(book_params)
@@ -23,8 +25,22 @@ class BooksController < ApplicationController
   def edit
   end
   
-  
-   def book_params
-    params.require("book").permit(:title, :intro, :author, :price)
+  def update
+ 
+    if @book.update(book_params)
+      redirect_to books_path, notice:"update!"
+    else
+    render 'edit'
+    end
   end
+  
+  private
+    def find_book
+      @book = Book.find_by(id: params[:id])
+      redirect_to books_path, notice:"no data!" if @book.nil?
+    end
+  
+    def book_params
+      params.require("book").permit(:title, :intro, :author, :price)
+    end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   validates :title,presence: true
-  has_many :logs
+  
   
 =begin
   def display_name
